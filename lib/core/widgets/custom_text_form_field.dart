398d4e049a6948939516d3f02a1ad45ea9b87389
MinaYasser100 <minayasser43@gmail.com>
2025-10-04 15:04:36 +0300

    fixed show validatioin

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -47,56 +47,89 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
   @override
   Widget build(BuildContext context) {
     final isFocused = _focusNode.hasFocus;
-    return Container(
-      decoration: isFocused
-          ? BoxDecoration(
-              border: Border.all(
-                color: ColorsTheme().primaryLight.withAlpha(
-                  153,
-                ), // Outer border color
-                width: 2, // Wider outer border
-              ),
-              borderRadius: BorderRadius.circular(18),
-            )
-          : null,
-      child: TextFormField(
-        controller: widget.textFieldModel.controller,
-        cursorColor: ColorsTheme().primaryDark,
-        validator: widget.textFieldModel.validator,
-        autovalidateMode: widget.textFieldModel.autovalidateMode,
-        obscureText: isObscured,
-        keyboardType: widget.textFieldModel.keyboardType,
-        autofocus: widget.textFieldModel.autofocus,
-        focusNode: _focusNode,
-        onFieldSubmitted: widget.textFieldModel.onFieldSubmitted,
-        style: TextStyle(color: ColorsTheme().primaryDark, fontSize: 14),
-        decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          hintText: widget.textFieldModel.hintText,
-          errorText: widget.textFieldModel.errorText,
-          hintStyle: TextStyle(color: ColorsTheme().primaryLight),
-          //labelStyle: TextStyle(color: ColorsTheme().primaryDark),
-          suffixIcon: widget.textFieldModel.obscureText
-              ? GestureDetector(
-                  onTap: () {
-                    isObscured = !isObscured;
-                    setState(() {});
-                  },
-                  child: Icon(
-                    isObscured
-                        ? Icons.visibility_outlined
-                        : Icons.visibility_off_outlined,
-                    color: isObscured
-                        ? ColorsTheme().primaryDark
-                        : ColorsTheme().primaryLight,
+
+    // Use FormField so we can render the error text outside the decorated
+    // container (below it) instead of letting InputDecoration draw it inside.
+    return FormField<String>(
+      initialValue: widget.textFieldModel.controller.text,
+      validator: widget.textFieldModel.validator,
+      autovalidateMode: widget.textFieldModel.autovalidateMode,
+      builder: (FormFieldState<String> fieldState) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: isFocused
+                  ? BoxDecoration(
+                      border: Border.all(
+                        color: ColorsTheme().primaryLight.withAlpha(
+                          153,
+                        ), // Outer border color
+                        width: 2, // Wider outer border
+                      ),
+                      borderRadius: BorderRadius.circular(18),
+                    )
+                  : null,
+              child: TextField(
+                controller: widget.textFieldModel.controller,
+                cursorColor: ColorsTheme().primaryDark,
+                onChanged: (v) => fieldState.didChange(v),
+                obscureText: isObscured,
+                keyboardType: widget.textFieldModel.keyboardType,
+                autofocus: widget.textFieldModel.autofocus,
+                focusNode: _focusNode,
+                onSubmitted: widget.textFieldModel.onFieldSubmitted,
+                style: TextStyle(
+                  color: ColorsTheme().primaryDark,
+                  fontSize: 14,
+                ),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 10,
                   ),
-                )
-              : null,
-          border: InputBorder.none,
-          focusedBorder: _customOutlineInputBorder(),
-          enabledBorder: InputBorder.none,
-        ),
-      ),
+                  hintText: widget.textFieldModel.hintText,
+                  // Hide the internal error text so we can show it below the
+                  // container instead.
+                  errorText: null,
+                  hintStyle: TextStyle(color: ColorsTheme().primaryLight),
+                  suffixIcon: widget.textFieldModel.obscureText
+                      ? GestureDetector(
+                          onTap: () {
+                            isObscured = !isObscured;
+                            setState(() {});
+                          },
+                          child: Icon(
+                            isObscured
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined,
+                            color: isObscured
+                                ? ColorsTheme().primaryDark
+                                : ColorsTheme().primaryLight,
+                          ),
+                        )
+                      : null,
+                  border: InputBorder.none,
+                  focusedBorder: _customOutlineInputBorder(),
+                  enabledBorder: InputBorder.none,
+                ),
+              ),
+            ),
+
+            // Render validation message below the two borders (outside the
+            // decorated container). We use the FormField's state to get the
+            // current error text so it'll update when the form is validated.
+            if (fieldState.errorText != null)
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, top: 6.0),
+                child: Text(
+                  fieldState.errorText!,
+                  style: TextStyle(color: Colors.red[700], fontSize: 12),
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 

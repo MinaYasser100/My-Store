9957dd0ce98bdc3dfb71aff7d795f56d8c40e8b3
MinaYasser100 <minayasser43@gmail.com>
2025-10-12 23:15:36 +0300

    do some edit in the home view

---
+++ lib/features/home/ui/custom_home_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomHomeAppBar extends StatelessWidget {
+  const CustomHomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      pinned: true,
+      expandedHeight: 80.0,
+      actionsPadding: EdgeInsets.all(10),
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 10.0, top: 4.0, bottom: 4.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Image.asset("assets/images/logo.png", fit: BoxFit.cover),
+        ),
+      ),
+      title: Text('My Store'),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            // Open search
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/features/home/ui/home_view.dart
+++ lib/features/home/ui/home_view.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:my_store/core/utils/colors.dart';
+
+import 'custom_home_app_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home View'));
+    return CustomScrollView(
+      slivers: [
+        CustomHomeAppBar(),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((
+            BuildContext context,
+            int index,
+          ) {
+            return Card(
+              color: ColorsTheme().whiteColor,
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomLeft: Radius.circular(12),
+                    ),
+                    child: Image.network(
+                      'https://picsum.photos/200/300?random=$index',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 12),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Product #$index',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          'Description for product item',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }, childCount: 20),
+        ),
+      ],
+    );
   }
 }
